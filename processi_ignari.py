37593ff5ebb12739ac5a8ddd99ab1314c5c352cf
mattefarii <149668857+mattefarii@users.noreply.github.com>
2024-02-08 10:10:59 +0100

    .

---
+++ processi_ignari.py
@@ -0,0 +1,23 @@
+from multiprocessing import Process
+"""
+multiprocessing è una libreria per la creazione, la comunicazione
+e la sincronizzazione tra processi nella programmazione parallela
+e concorrente.
+Process è una classe per creare processi eseguendo una funzione(o metodo)
+specificata come target."""
+
+def process_function(data):
+    result=data*2
+    print(result)
+
+if __name__=="__main__":
+    data_list=[1,2,3,4]
+    processes=[]
+    
+    for data in data_list:
+        p=Process(target=process_function, args=(data,))
+        processes.append(p)
+        p.start() #Avvia l'esecuzione del processo separato
+        
+    for p in processes:
+        p.join() #Blocca il processo principale fino a quando il processo separato non termina
---
+++ processi_ind_a_conoscenza.py
@@ -0,0 +1,42 @@
+import os
+from multiprocessing import Process,Queue,current_process
+
+"""Queue:classe che rappresenta una coda condivisa tra processi.
+    È utilizzata per consentire la comunicazione tra processi,
+    consentendo loro di scambiarsi dati in modo sicuro.
+    put(item):Aggiunge un elemento alla coda.
+    empty():Restituisce True se la coda è vuota, altrimenti restituisce False.
+    full():Restituisce True se la coda è piena, altrimenti restituisce False.
+    qsize():Restituisce il numero di elementi presenti nella coda.
+    close():Chiude la coda.
+    --current_process:funzione che restituisce un oggeetto Process
+    che rappresenta il processo in esecuzione."""
+    
+    
+def process_id():
+    print(f"Server PID:{os.getpid()}, Process Name:{current_process().name},Process PID: {current_process().pid}")
+    
+
+def process_function(data,result_queue):
+    process_id()
+    print("Elabora:",data)
+    result=data*2
+    result_queue.put(result)
+    
+if __name__=="__main__":
+    data_list=[1,2,3,4]
+    result_queue=Queue()
+    processes=[]
+    
+    for data in data_list:
+        p=Process(target=process_function, args=(data,result_queue))
+        processes.append(p)
+        p.start()
+        
+    for p in processes:
+        p.join()
+        
+    print("Il main stampa i risultati")
+    while not result.queue.empty():
+        result=result.queue.get()
+        print(result)
